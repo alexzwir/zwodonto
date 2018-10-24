cc1dbf65f69896dc8df36cb157b8d43b91b89325
alexzwir <alexzwir@gmail.com>
2018-10-24 08:34:20 -0300

    Adding the Contact Model and creating the basic fields. Importing to the Django Admin.

--- website/core/models.py
+++ website/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactModel(models.Model):
+    contact_name = models.CharField(max_length=30)
+    contact_email = models.CharField(max_length=30)
+    contact_phone = models.CharField(max_length=30)
+    contact_subject = models.CharField(max_length=30)
+    contact_message = models.CharField(max_length=30)
