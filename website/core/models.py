8e12bc7cf64b7f2869f0304d4d07ddce9c1d7825
alexzwir <alexzwir@gmail.com>
2018-10-24 08:37:20 -0300

    Creating the contact_send_date field on the model.

--- website/core/models.py
+++ website/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.encoding import smart_text
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,3 +10,11 @@ class ContactModel(models.Model):
     contact_phone = models.CharField(max_length=30)
     contact_subject = models.CharField(max_length=30)
     contact_message = models.CharField(max_length=30)
+    contact_sent_date = models.DateTimeField(max_length=35,default=timezone.now)
+
+    class Meta:
+        verbose_name = "Contato"
+        verbose_name_plural = "Contatos"
+
+    def __str__(self):
+        return smart_text("{} | {} | {}".format(self.contact_name,self.contact_sent_date,self.contact_email))
