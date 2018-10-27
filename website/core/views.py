69f2d817de71283fc62afd4162a2ee50945342ab
alexzwir <alexzwir@gmail.com>
2018-10-27 17:45:54 -0300

    Adding the AppoinmentForm on the template and adjusting the field on the class

--- website/core/views.py
+++ website/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .forms import ContactForm
+from .forms import ContactForm, AppoinmentForm
 
 # Create your views here.
 def home(request):
     form = ContactForm()
-    return render(request,"index.html",{"form":form})
+    form_appoinment = AppoinmentForm()
+    return render(request,"index.html",{"form":form,"form_appoinment":form_appoinment})
